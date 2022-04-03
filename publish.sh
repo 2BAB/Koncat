7be576b35ee6dc0ef167cd4c83b04bbba888d2fd
2BAB <xx2bab@gmail.com>
2022-04-03 21:18:34 +0800

    fix: refresh the publish shell scripts
    to publish all plugins

--- publish.sh
+++ publish.sh
@@ -1,10 +1,2 @@
-#!/usr/bin/env bash
-
-# Keep the order as followed one may depend on previous one
-MODULE_ARRAY=('koncat-contract' 'koncat-gradle-plugin' 'koncat-processor-api')
-for module in "${MODULE_ARRAY[@]}"
-do
-./gradlew :"$module":publishKoncatArtifactPublicationToSonatypeRepository
-done
-
+./gradlew clean publishAllPublicationsToSonatypeRepository
 ./gradlew releaseArtifactsToGithub
--- publish_to_local.sh
+++ publish_to_local.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-
-# Keep the order as followed one may depend on previous one
-MODULE_ARRAY=('koncat-contract' 'koncat-gradle-plugin' 'koncat-processor-api')
-for module in "${MODULE_ARRAY[@]}"
-do
-./gradlew clean :"$module":publishKoncatArtifactPublicationToMavenLocal
-done
+./gradlew clean publishToMavenLocal
