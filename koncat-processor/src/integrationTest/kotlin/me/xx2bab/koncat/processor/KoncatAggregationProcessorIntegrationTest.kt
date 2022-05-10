bf74f53edfda3d88ea323f826838ef39b3752b1d
2BAB <xx2bab@gmail.com>
2022-05-10 20:00:52 +0800

    clean: simplify the import of `is` for integration tests

--- koncat-processor/src/integrationTest/kotlin/me/xx2bab/koncat/processor/KoncatAggregationProcessorIntegrationTest.kt
+++ koncat-processor/src/integrationTest/kotlin/me/xx2bab/koncat/processor/KoncatAggregationProcessorIntegrationTest.kt
@@ -11,7 +11,7 @@ import me.xx2bab.koncat.contract.KONCAT_ARGUMENT_INTERMEDIATES_DIR
 import me.xx2bab.koncat.processor.fixture.generatedFileOfDummyLib
 import me.xx2bab.koncat.processor.fixture.sourcesForDummyApp
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.core.StringContains
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -37,7 +37,7 @@ class KoncatAggregationProcessorIntegrationTest {
 
         // Overall result
         val result = compilation.compile()
-        assertThat(result.exitCode, Matchers.`is`(KotlinCompilation.ExitCode.OK))
+        assertThat(result.exitCode, `is`(KotlinCompilation.ExitCode.OK))
 
         // Generated file check
         val koncatMetaForDummyApp = compilation.kspSourcesDir
@@ -46,7 +46,7 @@ class KoncatAggregationProcessorIntegrationTest {
             .first()
         assertThat(
             koncatMetaForDummyApp.name,
-            Matchers.`is`("KoncatAggregatedMeta1.kt")
+            `is`("KoncatAggregatedMeta1.kt")
         )
 
         val fileContent = koncatMetaForDummyApp.readText()
@@ -54,39 +54,39 @@ class KoncatAggregationProcessorIntegrationTest {
         println("[KoncatAggregationProcessorIntegrationTest] extension: $metaInJsonText")
         val metadata = Json.decodeFromString<KoncatProcMetadata>(metaInJsonText)
 
-        assertThat(metadata.annotatedClasses.size, Matchers.`is`(1))
+        assertThat(metadata.annotatedClasses.size, `is`(1))
         val suppressList = metadata.annotatedClasses["kotlin.Suppress"]!!
-        assertThat(suppressList.size, Matchers.`is`(2))
+        assertThat(suppressList.size, `is`(2))
         assertThat(
             suppressList[0].name,
-            Matchers.`is`("me.xx2bab.koncat.sample.AnnotatedClass")
+            `is`("me.xx2bab.koncat.sample.AnnotatedClass")
         )
         assertThat(
             suppressList[1].name,
-            Matchers.`is`("me.xx2bab.koncat.sample.AnnotatedClass2")
+            `is`("me.xx2bab.koncat.sample.AnnotatedClass2")
         )
         assertThat(
             suppressList[0].annotations[0].arguments.values.first(),
-            Matchers.`is`("[msg]")
+            `is`("[msg]")
         )
 
-        assertThat(metadata.typedClasses.size, Matchers.`is`(1))
+        assertThat(metadata.typedClasses.size, `is`(1))
         val runnableList = metadata.typedClasses["java.lang.Runnable"]!!
-        assertThat(runnableList.size, Matchers.`is`(2))
+        assertThat(runnableList.size, `is`(2))
         assertThat(
             runnableList[0],
-            Matchers.`is`("me.xx2bab.koncat.sample.DummyRunnable")
+            `is`("me.xx2bab.koncat.sample.DummyRunnable")
         )
         assertThat(
             runnableList[1],
-            Matchers.`is`("me.xx2bab.koncat.sample.DummyRunnable2")
+            `is`("me.xx2bab.koncat.sample.DummyRunnable2")
         )
 
-        assertThat(metadata.typedProperties.size, Matchers.`is`(1))
+        assertThat(metadata.typedProperties.size, `is`(1))
         val randomList = metadata.typedProperties["java.util.Random"]!!
-        assertThat(randomList.size, Matchers.`is`(2))
-        assertThat(randomList[0], Matchers.`is`("me.xx2bab.koncat.sample.prop"))
-        assertThat(randomList[1], Matchers.`is`("me.xx2bab.koncat.sample.prop2"))
+        assertThat(randomList.size, `is`(2))
+        assertThat(randomList[0], `is`("me.xx2bab.koncat.sample.prop"))
+        assertThat(randomList[1], `is`("me.xx2bab.koncat.sample.prop2"))
     }
 
 
@@ -108,7 +108,7 @@ class KoncatAggregationProcessorIntegrationTest {
 
         // Overall result
         val result = compilation.compile()
-        assertThat(result.exitCode, Matchers.`is`(KotlinCompilation.ExitCode.OK))
+        assertThat(result.exitCode, `is`(KotlinCompilation.ExitCode.OK))
 
         // Generated file check
         val koncatAggregationRouterForDummyApp = compilation.kspSourcesDir
@@ -117,7 +117,7 @@ class KoncatAggregationProcessorIntegrationTest {
             .first()
         assertThat(
             koncatAggregationRouterForDummyApp.name,
-            Matchers.`is`("KoncatAggregation.kt")
+            `is`("KoncatAggregation.kt")
         )
 
         val fileContent = koncatAggregationRouterForDummyApp.readText()
